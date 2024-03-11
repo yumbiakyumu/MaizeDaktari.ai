e8fecbf3d1f8362442f2a028a17c5415862c37ce
Boniface Kyumu <yumbiakyumu@gmail.com>
2024-03-11 14:33:17 +0300

    first commit

---
+++ Maizedaktari/assets/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+      for (let i = 0; i < elem.length; i++) {
+        elem[i].addEventListener(type, callback);
+      }
+    } else {
+      elem.addEventListener(type, callback);
+    }
+}
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+addEventOnElem(navTogglers, "click", toggleNavbar);
